fd7a545152fd252c8b37847b467d3a3d4afdfde9
LuisMolina <Luis.Molina1@alu.uclm.es>
2023-02-11 13:17:41 +0100

    Implementación inicial - v.0.0.1
    + Añadidas imágenes
    + Estructuración
    + Implementación botón subir vida

---
+++ MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace pokemon
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        DispatcherTimer dtTime;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void increaseHealth(object sender, object e)
+        {
+            this.pbHealth.Value += 0.4;
+            if (pbHealth.Value >= 100)
+            {
+                this.dtTime.Stop();
+                this.imgHealth.Opacity = 1;
+            }
+        }
+
+        private void btnHealth_Click(object sender, RoutedEventArgs e)
+        {
+            dtTime = new DispatcherTimer();
+            dtTime.Interval = TimeSpan.FromMilliseconds(100);
+            dtTime.Tick += increaseHealth;
+            dtTime.Start();
+            this.imgHealth.Opacity = 0.5;
+        }
+
+        private void btnEnergy_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        
+    }
+}
